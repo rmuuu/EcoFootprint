0a1574dd86327e7730136dbc187302b9e0cb7e4c
Ram Montoya <119645570+rmuuu@users.noreply.github.com>
2024-04-24 08:26:48 +0800

    Add files via upload

---
+++ Copy1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace EcoFootprint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.enter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(604, 138);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // enter
+            // 
+            this.enter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.enter.Location = new System.Drawing.Point(277, 147);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(87, 41);
+            this.enter.TabIndex = 1;
+            this.enter.Text = "Enter";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(651, 230);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EcoFootprint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button enter;
+    }
+}
+
---
+++ Copy1/Input.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EcoFootprint
+{
+    public partial class Input : Form
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            StreamWriter write = File.AppendText(Application.StartupPath + "\\storage\\" + "ecofootprint.txt");
+            
+            try
+            {
+                write.WriteLine(monthBox.Text + " " + yearBox.Text);
+                write.WriteLine("Location Size: " + sizeBox.Text);
+                write.WriteLine("Population:" + population.Text);
+                write.WriteLine("Electric Consumption: " + bill.Text);
+                write.WriteLine("Gasoline Consumption: " + gas.Text);
+                write.WriteLine("Waste Disposed: " + waste.Text);
+                write.WriteLine("Materials Recycled: " + recycle.Text);
+
+                int footprint = ((int.Parse(bill.Text) / 277800) * 20) + (int.Parse(gas.Text) * 18);
+                if (int.Parse(recycle.Text) > 1)
+                {
+                    footprint += 260;
+                }
+                write.WriteLine("Carbon Footprint: " + footprint);
+                write.WriteLine();
+
+                MessageBox.Show("Saved Successfully.\nCalculated Carbon Footprint = " + footprint);
+                write.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
