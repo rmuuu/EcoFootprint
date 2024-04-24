4dc4fe8e2661c49dc94148d88deced19a3299890
Ram Montoya <119645570+rmuuu@users.noreply.github.com>
2024-04-24 09:42:09 +0800

    Add files via upload

---
+++ Copy2/Input.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EcoFootprint
+{
+    public partial class Input : Form
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            StreamWriter write = File.AppendText(Application.StartupPath + "\\storage\\" + "ecofootprint.csv");
+            
+            try
+            {
+                write.Write(monthBox.Text + " " + yearBox.Text + ",");
+                write.Write("Location Size: " + sizeBox.Text + ",");
+                write.Write("Population:" + population.Text + ",");
+                write.Write("Electric Consumption: " + bill.Text + ",");
+                write.Write("Gasoline Consumption: " + gas.Text + ",");
+                write.Write("Waste Disposed: " + waste.Text + ",");
+                write.Write("Materials Recycled: " + recycle.Text + ",");
+
+
+                int footprint = ((int.Parse(bill.Text) / 277800) * 20) + (int.Parse(gas.Text) * 18);
+                if (int.Parse(recycle.Text) > 1)
+                {
+                    footprint += 260;
+                }
+                write.Write("Carbon Footprint: " + footprint + ",");
+                write.WriteLine();
+
+                MessageBox.Show("Saved Successfully.\nCalculated Carbon Footprint = " + footprint);
+                write.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Copy2/Search.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace EcoFootprint
+{
+    public partial class Search : Form
+    {
+        public Search()
+        {
+            InitializeComponent();
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Application.StartupPath + "\\storage\\" + "ecofootprint.csv";
+            ofd.Filter = "CSV | *.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = File.ReadAllLines(Application.StartupPath + "\\storage\\" + "ecofootprint.csv");
+                
+                foreach (string s in lines)
+                {
+                    if (searchBox.Text == s)
+                    {
+                        textBox.AppendText(s + "\n");
+                        lines = s.Split(',');
+                    }
+                }
+            }
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
+        }
+    }
+}
